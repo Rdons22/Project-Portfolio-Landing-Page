9ec21ac8e7188cc6999dae6cb4a25bd0a6d0ef4b
Ryan Donaldson <ryandonaldson40@gmail.com>
2026-05-13 22:56:47 -0700

    Configure GitHub Pages deployment

--- Project-Portfolio-Landing-Page-main/vite.config.js
+++ Project-Portfolio-Landing-Page-main/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Project-Portfolio-Landing-Page/",
   plugins: [react()],
   server: { port: 5173, host: "127.0.0.1" },
 });
